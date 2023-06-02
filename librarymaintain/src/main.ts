a81858ac322f98fdfb8f24f175283a4100a71fe1
Tauseef Ahmad <tauseefalpha@gmail.com>
2023-06-02 11:03:01 +0500

    cors enabled

--- librarymaintain/src/main.ts
+++ librarymaintain/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule,{cors:true});
-  await app.listen(process.env.PORT);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: [
+      'https://fullstack-bp58.vercel.app',
+      'http://localhost:3000',
+      'http://localhost:3001',
+    ],
+  });
+
+  const PORT = process.env.PORT || 3005;
+  await app.listen(PORT, async () => {
+    console.log(`listening on port  ${PORT}`);
+  });
 }
 bootstrap();
